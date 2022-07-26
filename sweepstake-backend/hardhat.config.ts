3f0c0216fb5da00037192c6259ec95152cdb0d74
sarthak <sarthak.ahuja0007@gmail.com>
2022-07-26 20:48:06 +0530

    hardaht config

--- sweepstake-backend/hardhat.config.ts
+++ sweepstake-backend/hardhat.config.ts
@@ -1,6 +1,8 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-require("hardhat-deploy");
+import "hardhat-deploy";
+import "hardhat-gas-reporter";
+require("dotenv").config();
 
 const config: HardhatUserConfig = {
 	solidity: "0.8.9",
@@ -19,11 +21,23 @@ const config: HardhatUserConfig = {
 			chainId: 80001,
 			accounts: [process.env.ACCOUNT_1!, process.env.ACCOUNT_2!]
 		},
-        goerli:{
-            url: process.env.GOERLI_RPC_URL,
-            chainId: 80002,
-            accounts: [process.env.ACCOUNT_1!, process.env.ACCOUNT_2!]
-        }
+		goerli: {
+			url: process.env.GOERLI_RPC_URL,
+			chainId: 80002,
+			accounts: [process.env.ACCOUNT_1!, process.env.ACCOUNT_2!]
+		}
+	},
+	etherscan: {
+		apiKey: process.env.ETHERSCAN_API_KEY
+	},
+	gasReporter: {
+		enabled: true,
+		currency: "INR",
+		token: "MATIC",
+		coinmarketcap: process.env.COINMARKETCAP_API_KEY
+	},
+	namedAccounts: {
+		deployer: 0
 	}
 };
 
